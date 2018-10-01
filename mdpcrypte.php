5bcaf91b73fc5d6c756dcffa6eccb8781fb22a5d
master <mostowyjb@gmail.com>
2018-10-01 16:11:48 +0200

    script mdp crypyter

---
+++ mdpcrypte.php
@@ -0,0 +1,31 @@
+<?php
+
+    try
+    {
+            $bdd=new PDO('mysql:host=localhost;dbname=gsbapplifrais;charset=utf8','root','',
+            array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+
+    }
+    catch(Exception $e)
+    {
+            echo'Echec de connexion à la BDD car';
+            die('Erreur :'.$e->getMessage());
+    }
+    
+    $req = $bdd->prepare("SELECT id,mdp FROM utilisateurs");
+    $req->execute();
+    $res=$req->fetchAll();
+    
+    foreach($res as $utilisateur)
+    {
+        $mdpCrypte= password_hash($utilisateur['mdp'], PASSWORD_BCRYPT);
+        $req = $bdd-> prepare("UPDATE utilisateurs SET mdpCrypte=:mdpCrypte WHERE id=:id");
+        $req->bindParam(':mdpCrypte',$mdpCrypte);
+        $req->bindParam(':id',$utilisateur['id']);
+        $req->execute();
+        
+    }
+
+
+
+?>
